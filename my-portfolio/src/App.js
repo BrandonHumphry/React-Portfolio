69d53311d041536346a5b72aac1af211664d033a
Brandon Humphry <brandonhumphry@gmail.com>
2020-08-12 19:17:11 -0700

    portfolio links, grid, and contact page updates

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import Home from './pages/home'
 import Portfolio from './pages/portfolio';
 import Contact from './pages/contact';
+import DevelopmentGrid from './components/developmentgrid';
+import DesignGrid from './components/designgrid';
 
 
 function App() {
@@ -13,7 +15,12 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
+          {/* portfolio page and routes */}
           <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/development" component={DesignGrid} />
+          <Route exact path="/design" component={DevelopmentGrid} />
+          {/* add individual page routes below */}
+          {/* <Route exact path="/WholeFoods" component={WholeFoods} /> */}
           <Route exact path="/contact" component={Contact} />
         </Switch>
       </div>
--- my-portfolio/src/pages/portfolio.js
+++ my-portfolio/src/pages/portfolio.js
@@ -1,15 +1,6 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "../components/navigation";
-// import ImgMediaCard from '../components/card';
-import Thumbnail from "../components/thumbnail";
-import Grid from "@material-ui/core/Grid";
-
-const gridContainer = {
-    maxWidth: "1250px",
-    paddingBottom: "50px",
-    margin: "auto"
-}
+import {Link} from "react-router-dom"
 
 function Portfolio() {
   useEffect(() => {
@@ -18,38 +9,9 @@ function Portfolio() {
   return (
     <div className="App">
       <Navigation></Navigation>
-      <Grid item sm={12} container justify="center" style={gridContainer}>
-        <Thumbnail
-          link="/instacart"
-          image="assets/portfolio/instacart.png"
-          title="Instacart UI Designs"  
-        />
-        <Thumbnail
-          link="/instacart"
-          image="assets/portfolio/instacart.png"
-          title="Instacart UI Designs"
-        />
-        <Thumbnail
-          link="/instacart"
-          image="assets/portfolio/instacart.png"
-          title="Instacart UI Designs"
-        />
-        <Thumbnail
-          link="/instacart"
-          image="assets/portfolio/instacart.png"
-          title="Instacart UI Designs"
-        />
-        <Thumbnail
-          link="/instacart"
-          image="assets/portfolio/instacart.png"
-          title="Instacart UI Designs"
-        />
-        <Thumbnail
-          link="/instacart"
-          image="assets/portfolio/instacart.png"
-          title="Instacart UI Designs"
-        />
-      </Grid>
+      {/* area for sub nav for Design and Development: links to routes for different Grid components */}
+      <Link to="/design">Design</Link>
+      <Link to="/development">Development</Link>
     </div>
   );
 }
