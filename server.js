c132e89d02c2263e78bdad80e0520674421b9078
Brandon Humphry <brandonhumphry@gmail.com>
2020-08-15 09:13:36 -0700

    modifying server.js

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const users = require("./routes/api/users");
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080;
+
 const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+ });
 
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-
-app.use(bodyParser.json());
-
-const db = require("./config/keys").mongoURI;
-
-mongoose
-  .connect(process.env.MONGO_URI || db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-app.use(passport.initialize());
-require("./config/passport")(passport);
-app.use("/api/users", users);
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server up on port ${port} !`));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
